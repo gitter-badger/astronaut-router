e28158e25c4359b6dfa1f41d94daf35fb5f9e52c
Guilherme Diego Albino Francisco <guidiego.expgames@gmail.com>
2015-11-27 07:43:26 -0200

    Add first functions of Convertion Middleware

--- bin/convert.js
+++ bin/convert.js
@@ -1,12 +1,74 @@
 function Convert(app, type, controllers, middlewares) {
+  var fs = require("fs"),
+      globalLoadedMiddlewares = {};
 
-  require("fs").readdirSync(controllers).forEach(function(file) {
+  /**
+   * This function register global middlewares, and construct middleObject
+   */
+  fs.readdirSync(middlewares).forEach(function (file) {
+    if (file.indexOf(".js") > -1) {
+      //Start Try
+      try {
+        require.resolve(middlewares + file);
+        var mid = require(middlewares + file);
+
+        if (typeof mid == 'function') {
+          app.use(mid);
+        } else if (typeof mid == 'object') {
+
+        } else {
+          console.log('[Type Error : Middleware need be a Object/Function]');
+        }
+      } catch (e) {
+        console.log(e);
+      }
+      //End Try
+    } else {
+      globalLoadedMiddlewares[file] = {all:[], get:[], post:[], put:[], delete:[]};
+      fs.readdirSync(middlewares + file).forEach(function (js) {
+        //Start Try
+        try {
+          require.resolve(middlewares + file + "/" + js);
+          var mid = require(middlewares + file + "/" + js);
+
+          if (typeof mid == 'function') {
+            globalLoadedMiddlewares[file].all.push(mid);
+          } else if (typeof mid == 'object') {
+            if (mid.include) {
+              mid.include.forEach(function(method) {
+                globalLoadedMiddlewares[file][method].push(mid.fn);
+              });
+            } else if (mid.exclude) {
+              Object.keys(globalLoadedMiddlewares[file]).forEach(function (k) {
+                if (mid.exclude.indexOf(k) || mid.exclude.indexOf(k.toUpperCase()))
+                  globalLoadedMiddlewares[file][k] = mid.fn;
+              });
+            } else {
+              console.log("[Miss Error : You need put a property exclude or include in Object Middlewares]");
+            }
+          } else {
+            console.log('[Type Error : Middleware need be a Object/Function]');
+          }
+        } catch (e) {
+          console.log(e);
+        }
+        //End Try
+      });
+    }
+  });
+
+  /**
+   * This function MAP the routes by filess
+   */
+  fs.readdirSync(controllers).forEach(function(file) {
     try {
       require.resolve(controllers + file);
 
-      var controoller  = null,
-          url          = "/" + file.split("_")[0],
-          _require     = require(controllers + file);
+      var controller   = null,
+          __name       = file.split("_")[0],
+          url          = "/" + __name,
+          _require     = require(controllers + file),
+          mid          = globalLoadedMiddlewares[__name] ? globalLoadedMiddlewares[__name] : {get:[], post:[], put:[], delete:[]};
 
       if (typeof _require == 'function')
         controller = new _require();
@@ -15,13 +77,15 @@ function Convert(app, type, controllers, middlewares) {
       else
         console.log('[Type Error : Controller need be a Object/Function]');
 
-
+      if (mid.all)
+        app.all(url, mid.all);
+      
       if (type == 'rest') {
-        if (controller.findById) {app.get(url + "/:id", controller.findById); delete controller.findById}
-        if (controller.find) {app.get(url, controller.find); delete controller.find};
-        if (controller.create) {app.post(url, controller.create); delete controller.create}
-        if (controller.update) {app.put(url + "/:id", controller.update);delete controller.update}
-        if (controller.remove) {app.delete(url + "/:id", controller.remove); delete controller.remove}
+        if (controller.findById) {app.get(url + "/:id", mid.get, controller.findById); delete controller.findById}
+        if (controller.find) {app.get(url, mid.get, controller.find); delete controller.find};
+        if (controller.create) {app.post(url, mid.post, controller.create); delete controller.create}
+        if (controller.update) {app.put(url + "/:id", mid.put, controller.update);delete controller.update}
+        if (controller.remove) {app.delete(url + "/:id", mid.delete, controller.remove); delete controller.remove}
 
         var keys = Object.keys(controller);
 
@@ -31,7 +95,7 @@ function Convert(app, type, controllers, middlewares) {
                 method = kArr[0].toLowerCase(),
                 urlCom = kArr[1];
 
-            app[method](url + "/" + urlCom, controller[k]);
+            app[method](url + "/" + urlCom, mid[method], controller[k]);
           });
         }
       }
@@ -42,7 +106,7 @@ function Convert(app, type, controllers, middlewares) {
               method = kArr[0].toLowerCase(),
               urlCom = kArr[1];
 
-          app[method](url + "/" + urlCom, controller[k]);
+          app[method](url + "/" + urlCom, mid[method], controller[k]);
         });
       }
 
